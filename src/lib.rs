7d98c40350916746d7cb8fed2b5105b1e6129ff8
Yiran Sheng <shengyiran@gmail.com>
2019-01-03 14:59:08 -0800

    move module

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
-mod compiler;
-mod error_print;
-mod ir;
-mod parser;
-mod scanner;
-mod vm;
+// mod compiler;
+// mod error_print;
+// mod ir;
+// mod parser;
+// mod scanner;
+// mod vm;
 
-pub mod ast;
+// pub mod ast;
 
-pub use crate::ast::{Func, Variable};
-pub use crate::compiler::{
-    compile, CompileError, CompileErrorInner, Compiler, Target,
-};
-pub use crate::error_print::InterpreterError;
-pub use crate::ir::{
-    Instruction, InstructionKind, Label, Visitor as IRVisitor,
-};
-pub use crate::parser::{Error as ParseError, Parser};
-pub use crate::scanner::{Scanner, SourceLoc, SourceMapped};
-pub use crate::vm::{RuntimeError, VM};
+// pub use crate::ast::{Func, Variable};
+// pub use crate::compiler::{
+// compile, CompileError, CompileErrorInner, Compiler, Target,
+// };
+// pub use crate::error_print::InterpreterError;
+// pub use crate::ir::{
+// Instruction, InstructionKind, Label, Visitor as IRVisitor,
+// };
+// pub use crate::parser::{Error as ParseError, Parser};
+// pub use crate::scanner::{Scanner, SourceLoc, SourceMapped};
+// pub use crate::vm::{RuntimeError, VM};
--- src/main.rs
+++ src/main.rs
@@ -8,71 +8,74 @@ use rand::FromEntropy;
 use structopt::StructOpt;
 
 mod ast;
-mod compiler;
-mod error_print;
+// mod compiler;
+mod compile;
+// mod error_print;
 mod ir;
 mod parser;
 mod scanner;
-mod vm;
+// mod vm;
 
-#[cfg(test)]
-mod tests;
+// #[cfg(test)]
+// mod tests;
 
-use crate::compiler::compile;
-use crate::error_print::{print_source_error, InterpreterError};
-use crate::parser::Parser;
-use crate::scanner::Scanner;
+// use crate::compiler::compile;
+// use crate::error_print::{print_source_error, InterpreterError};
+// use crate::parser::Parser;
+// use crate::scanner::Scanner;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "basic_rs", about = "basic_rs file")]
-struct Opt {
-    /// Input file
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
+// #[derive(Debug, StructOpt)]
+// #[structopt(name = "basic_rs", about = "basic_rs file")]
+// struct Opt {
+// /// Input file
+// #[structopt(parse(from_os_str))]
+// input: PathBuf,
 
-    #[structopt(short = "d", long = "disassemble")]
-    disassemble: bool,
-}
+// #[structopt(short = "d", long = "disassemble")]
+// disassemble: bool,
+// }
 
-fn read_source(opt: &Opt) -> Result<String, InterpreterError> {
-    let mut file = File::open(&opt.input)?;
-    let mut source = String::new();
-    file.read_to_string(&mut source)?;
+// fn read_source(opt: &Opt) -> Result<String, InterpreterError> {
+// let mut file = File::open(&opt.input)?;
+// let mut source = String::new();
+// file.read_to_string(&mut source)?;
 
-    Ok(source)
-}
+// Ok(source)
+// }
 
-fn run(source: &str, opt: &Opt) -> Result<(), InterpreterError> {
-    let scanner = Scanner::new(source);
-    let ast = Parser::new(scanner).parse()?;
+// fn run(source: &str, opt: &Opt) -> Result<(), InterpreterError> {
+// let scanner = Scanner::new(source);
+// let ast = Parser::new(scanner).parse()?;
 
-    let mut vm = compile(&ast)?;
+// let mut vm = compile(&ast)?;
 
-    let stdout = io::stdout();
+// let stdout = io::stdout();
 
-    if opt.disassemble {
-        vm.disassemble(stdout.lock());
-    }
+// if opt.disassemble {
+// vm.disassemble(stdout.lock());
+// }
 
-    let mut rng = SmallRng::from_entropy();
+// let mut rng = SmallRng::from_entropy();
 
-    vm.run(stdout.lock(), &mut rng)?;
+// vm.run(stdout.lock(), &mut rng)?;
 
-    Ok(())
-}
+// Ok(())
+// }
 
-fn main() {
-    let opt = Opt::from_args();
-    let source = match read_source(&opt) {
-        Ok(s) => s,
-        Err(err) => return eprintln!("{}", err),
-    };
+// fn main() {
+// let opt = Opt::from_args();
+// let source = match read_source(&opt) {
+// Ok(s) => s,
+// Err(err) => return eprintln!("{}", err),
+// };
 
-    match run(&source, &opt) {
-        Ok(_) => {}
-        Err(e) => match e {
-            InterpreterError::ParseFail(e) => print_source_error(e, &source),
-            _ => eprintln!("{}", e),
-        },
-    }
-}
+// match run(&source, &opt) {
+// Ok(_) => {}
+// Err(e) => match e {
+// InterpreterError::ParseFail(e) => print_source_error(e, &source),
+// _ => eprintln!("{}", e),
+// },
+// }
+// }
+
+fn main() {}
