0e19cdc41694fd16b3a3bae6e80391d0de989637
Yiran Sheng <shengyiran@gmail.com>
2019-01-04 16:54:23 -0800

    uncomment lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-mod compile;
-// mod error_print;
 mod codegen;
+mod compile;
+mod error_print;
 mod parser;
 mod scanner;
 mod vm;
@@ -8,9 +8,9 @@ mod vm;
 pub mod ast;
 pub mod ir;
 
-pub use crate::compile::{compile, CompileError};
-// pub use crate::error_print::InterpreterError;
 pub use crate::codegen::{codegen, WriteError};
+pub use crate::compile::{compile, CompileError};
+pub use crate::error_print::InterpreterError;
 pub use crate::parser::{Error as ParseError, Parser};
 pub use crate::scanner::{Scanner, SourceLoc, SourceMapped};
 pub use crate::vm::{RuntimeError, VM};
